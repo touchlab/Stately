8624e0d337ab5dc44290d56b6f390981fffb88d2
Kevin Galligan <kgalligan@gmail.com>
2018-10-13 11:58:15 -0400

    Save before removing remove

---
+++ src/macosMain/kotlin/co/touchlab/stately/LinkedList.kt
@@ -0,0 +1,248 @@
+package co.touchlab.stately
+
+import platform.Foundation.NSLock
+import kotlin.native.concurrent.AtomicInt
+import kotlin.native.concurrent.AtomicReference
+import kotlin.native.concurrent.freeze
+
+/**
+ * Linked List (doubly link). A linked list is a data structure consisting
+ * of a group of nodes which together represent a sequence.
+ *
+ *
+ * @see [Linked List
+ * @author Justin Wetherell <phishman3579></phishman3579>@gmail.com>
+](https://en.wikipedia.org/wiki/Linked_list) */
+class DoublyLinkedList<T> {
+
+    class Node<T>(val list:DoublyLinkedList<T>, val nodeValue: T) {
+
+        val prev = AtomicReference<Node<T>?>(null)
+        val next = AtomicReference<Node<T>?>(null)
+
+        fun add(t:T):Boolean{
+            val ins = Node<T>(list, t)
+
+            ins.freeze()
+
+            ins.prev.value = prev.value
+            ins.next.value = this
+            prev.value?.next?.value = ins
+            prev.value = ins
+
+            //If replacing 0
+            if(ins.prev.value == null)
+                list.head.value = ins
+
+            list.size.increment()
+
+            return true
+        }
+
+        fun remove(){
+            val prevNode = prev.value
+            val nextNode = next.value
+            if(prevNode == null)
+            {
+                list.head.value = nextNode
+            }
+            else
+            {
+                prevNode.next.value = nextNode
+            }
+
+            if(nextNode == null)
+            {
+                list.tail.value = prevNode
+            }
+            else
+            {
+                nextNode.prev.value = prevNode
+            }
+
+            list.size.decrement()
+        }
+    }
+
+    private val lock = NSLock()
+    private val size = AtomicInt(0)
+    private val head = AtomicReference<Node<T>?>(null)
+    private val tail = AtomicReference<Node<T>?>(null)
+
+    /**
+     * {@inheritDoc}
+     */
+    fun add(value: T): Boolean {
+        return add(Node(this, value))
+    }
+
+    fun nodeAt(i:Int):Node<T>{
+        if(i>=size.value)
+            throw IllegalArgumentException("index $i ge ${size.value}")
+
+        var node = head.value
+        var nodeCount = 0
+        while (node != null) {
+            if(i == nodeCount++)
+                return node
+            node = node.next.value
+        }
+
+        throw IllegalStateException("Bad math")
+    }
+
+    /**
+     * Add node to list.
+     *
+     * @param node
+     * to add to list.
+     */
+    private fun add(node: Node<T>): Boolean {
+        node.freeze()
+        if (isEmpty) {
+            head.value = node
+            tail.value = node
+        } else {
+            val prev = tail.value
+            prev!!.next.value = node
+            node.prev.value = prev
+            tail.value = node
+        }
+        size.increment()
+        return true
+    }
+
+    private val isEmpty:Boolean
+    get() = head.value == null
+
+
+    /**
+     * {@inheritDoc}
+     */
+    fun remove(value: T): Boolean {
+        // Find the node
+        var node = head.value
+        while (node != null && node.nodeValue != value) {
+            node = node.next.value
+        }
+        if (node == null)
+            return false
+
+        // Update the tail, if needed
+        if (node == tail.value)
+            tail.value = node.prev.value
+
+        val prev = node.prev.value
+        val next = node.next.value
+        if (prev != null && next != null) {
+            prev.next.value = next
+            next.prev.value = prev
+        } else if (prev != null && next == null) {
+            prev.next.value = null
+        } else if (prev == null && next != null) {
+            // Node is the head
+            next.prev.value = null
+            head.value = next
+        } else {
+            // prev==null && next==null
+            head.value = null
+        }
+        size.decrement()
+        return true
+    }
+
+    fun toList():List<T> {
+        val outlist = ArrayList<T>(size.value)
+        var node = head.value
+        while (node != null) {
+            outlist.add(node.nodeValue)
+            node = node.next.value
+        }
+
+        return outlist
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    fun clear() {
+        head.value = null
+        size.value = 0
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    fun contains(value: T): Boolean {
+        var node = head.value
+        while (node != null) {
+            if (node.nodeValue == value)
+                return true
+            node = node.next.value
+        }
+        return false
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    fun size(): Int {
+        return size.value
+    }
+
+    fun debugPrint():String{
+        val sb = StringBuilder()
+        toList().forEach {
+            sb.append("$it\n")
+        }
+
+        return sb.toString()
+    }
+
+    /*private inline fun <T> withLock(proc:() -> T):T{
+        lock.lock()
+        try {
+            return proc.invoke()
+        } finally {
+            lock.unlock()
+        }
+    }*/
+
+    /**
+     * {@inheritDoc}
+     */
+   /* fun validate(): Boolean {
+        val keys = java.util.HashSet()
+        val node = head
+        if (node != null) {
+            keys.add(node.value)
+            if (node.prev != null)
+                return false
+            var child = node.next
+            while (child != null) {
+                if (!validate(child, keys))
+                    return false
+                child = child.next
+            }
+        }
+        return keys.size == size
+    }*/
+
+    /*private fun validate(node: Node<T>, keys: MutableSet<T>): Boolean {
+        if (node.value == null)
+            return false
+
+        keys.add(node.value)
+
+        val child = node.next
+        if (child != null) {
+            if (child.prev != node)
+                return false
+        } else {
+            if (node != tail)
+                return false
+        }
+        return true
+    }*/
+}
+
---
+++ src/macosTest/kotlin/co/touchlab/stately/LinkedListTest.kt
@@ -0,0 +1,148 @@
+package co.touchlab.stately
+
+import kotlin.native.concurrent.*
+import kotlin.test.*
+
+class LinkedListTest{
+    @Test
+    fun add(){
+        val ll = makeTen()
+
+        assertEquals(10, ll.size())
+    }
+
+    @Test
+    fun remove(){
+        val ll = makeTen()
+
+        assertTrue(ll.remove(ListData("Item 4")))
+        assertTrue(ll.remove(ListData("Item 8")))
+        assertFalse(ll.remove(ListData("Item 8")))
+        assertFalse(ll.remove(ListData("Item 88")))
+
+        assertEquals(8, ll.size())
+    }
+
+    @Test
+    fun nodeAt(){
+        val ll = makeTen()
+
+        assertEquals(ll.nodeAt(5).nodeValue.s, "Item 5")
+        try {
+            ll.nodeAt(10)
+            fail("Should've been IllegalArgumentException")
+        }catch (e:IllegalArgumentException){}
+
+        val empty = DoublyLinkedList<ListData>()
+        try {
+            empty.nodeAt(0)
+            fail("Should've been IllegalArgumentException")
+        } catch (e: IllegalArgumentException) {}
+    }
+
+    @Test
+    fun nodeAdd(){
+        val ll = makeTen()
+
+        ll.nodeAt(2).add(ListData("asdf"))
+        assertEquals(ll.size(), 11)
+        assertEquals(ll.nodeAt(2).nodeValue.s, "asdf")
+        assertEquals(ll.nodeAt(3).nodeValue.s, "Item 2")
+
+        ll.nodeAt(0).add(ListData("a"))
+        ll.nodeAt(0).add(ListData("b"))
+
+        assertEquals(ll.size(), 13)
+        assertEquals(ll.nodeAt(0).nodeValue.s, "b")
+        assertEquals(ll.nodeAt(1).nodeValue.s, "a")
+
+        ll.nodeAt(12).add(ListData("c"))
+        ll.nodeAt(13).add(ListData("d"))
+
+        println(ll.debugPrint())
+
+        assertEquals(ll.size(), 15)
+        assertEquals(ll.nodeAt(12).nodeValue.s, "c")
+        assertEquals(ll.nodeAt(13).nodeValue.s, "d")
+    }
+
+    @Test
+    fun nodeRemove(){
+        val ll = makeTen()
+
+        ll.nodeAt(5).remove()
+        assertEquals(9, ll.size())
+
+        ll.nodeAt(0).remove()
+        assertEquals(8, ll.size())
+
+        try {
+            ll.nodeAt(8).remove()
+            fail("Shouldn't have this many values")
+        } catch (e: Exception) {
+
+        }
+
+        val node = ll.nodeAt(7)
+        assertEquals("Item 9", node.nodeValue.s)
+
+        node.remove()
+        assertEquals(7, ll.size())
+
+        assertEquals("Item 8", ll.nodeAt(6).nodeValue.s)
+
+        var loopCount = 20
+        while(ll.size() > 0){
+            println(ll.debugPrint())
+            ll.nodeAt(0).remove()
+            if(loopCount-- == 0){
+                throw IllegalStateException("Something went wrong. Give up.")
+            }
+        }
+    }
+
+//    @Test
+    fun multipleThreads(){
+        val ll = DoublyLinkedList<ListData>().freeze()
+        val workers = Array(5){Worker.start()}
+        val futures = Array(workers.size){
+            val worker = workers[it]
+            worker.execute(TransferMode.SAFE, {Pair(it, ll)}){
+                it.second.add(ListData("${it.first} 1"))
+                it.second.add(ListData("${it.first} 2"))
+                it.second.add(ListData("${it.first} 1"))
+            }
+        }
+
+        val futureSet = futures.toSet()
+        var consumed = 0
+        while (consumed < futureSet.size) {
+            val ready = futureSet.waitForMultipleFutures(10000)
+            ready.forEach {
+                it.consume { result ->
+                    consumed++
+                }
+            }
+        }
+
+        workers.forEach {
+            it.requestTermination().result
+        }
+
+        ll.toList().forEach {
+            println(it)
+        }
+
+        assertEquals(5*2, ll.size())
+    }
+
+    private fun makeTen(): DoublyLinkedList<ListData> {
+        val ll = DoublyLinkedList<ListData>()
+        for (i in 0 until 10) {
+            ll.add(ListData("Item $i"))
+        }
+        return ll
+    }
+}
+
+data class ListData(val s:String)
