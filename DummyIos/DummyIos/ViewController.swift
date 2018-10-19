7220c32de1559a96fce4fb07294217fb83c675f7
Kevin Galligan <kgalligan@gmail.com>
2018-10-19 14:22:53 -0400

    Adding dummy ios project to support CI

---
+++ DummyIos/DummyIos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DummyIos
+//
+//  Created by Kevin Galligan on 10/19/18.
+//  Copyright © 2018 Kevin Galligan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
