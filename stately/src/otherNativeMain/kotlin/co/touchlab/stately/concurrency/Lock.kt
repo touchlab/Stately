78292c4f5579b633086bfddde2aadf31307da972
Daniel Baird <daniel.baird@plangrid.com>
2019-04-19 10:22:44 -0700

    Make the native lock reentrant

--- stately/src/otherNativeMain/kotlin/co/touchlab/stately/concurrency/Lock.kt
+++ stately/src/otherNativeMain/kotlin/co/touchlab/stately/concurrency/Lock.kt
@@ -1,9 +1,21 @@
 package co.touchlab.stately.concurrency
 
+import co.touchlab.stately.freeze
 import kotlin.native.concurrent.AtomicInt
+import kotlin.native.concurrent.ThreadLocal
 
+@ThreadLocal
+private object Thread {
+  val id = Any().freeze().hashCode()
+}
+
+/*
+ * This is a re-entrant implementation of Lock.
+ * a thread may acquire the lock as many times as the wish.
+ */
 actual class Lock actual constructor() {
-  val lock = AtomicInt(0)
+  private val lock = AtomicInt(0)
+  private val reenterCount = AtomicInt(0)
 
   actual fun lock() {
     spinLock()
@@ -14,15 +26,33 @@ actual class Lock actual constructor() {
   }
 
   actual fun tryLock():Boolean {
-    return lock.compareAndSet(0, 1)
+    return when (lock.compareAndSwap(0, Thread.id)) {
+      Thread.id -> {
+        // We already have the lock, we are just re-entering.
+        reenterCount.increment()
+        true
+      }
+      0 -> {
+        // Another thread didn't have the lock so we just took it.
+        assert(reenterCount.value == 0)
+        true
+      }
+      else -> false
+    }
   }
 
-  private fun spinLock(){
-    while (!lock.compareAndSet(0, 1)){}
+  private fun spinLock() {
+    while (!tryLock()) {}
   }
 
   private fun spinUnlock(){
-    if(!lock.compareAndSet(1, 0))
-      throw IllegalStateException("Lock must always be 1 in unlock")
+    // Because this is re-entrant we should only unlock if the count is 0.
+    if (reenterCount.value > 0) {
+      reenterCount.decrement()
+    } else {
+      val currentThread = Thread.id
+      val lastThread = lock.compareAndSwap(currentThread, 0)
+      assert(currentThread == lastThread)
+    }
   }
 }
