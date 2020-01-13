7fd925bcf3671914fb779e5554740f7bba94a4f7
Kevin Schildhorn <kevinschildhorn@Kevin-Ss-MacBook-Pro.local>
2020-01-13 09:20:31 -0500

    Updating IsoMutableMap

--- stately-isolate/src/commonMain/kotlin/co/touchlab/stately/isolate/IsoMutableMap.kt
+++ stately-isolate/src/commonMain/kotlin/co/touchlab/stately/isolate/IsoMutableMap.kt
@@ -1,46 +1,84 @@
 package co.touchlab.stately.isolate
 
-class IsoMutableMap<K, V>() : IsolateState<MutableMap<K, V>>({ mutableMapOf() }), MutableMap<K, V> {
+import co.touchlab.stately.ensureNeverFrozen
+import co.touchlab.stately.isFrozen
+import co.touchlab.stately.isNativeFrozen
+
+class IsoMutableMap<K, V> : IsolateState<MutableMap<K, V>>({ mutableMapOf() }), MutableMap<K, V> {
+
+    init {
+        println("Ensure Never Frozen")
+        if(isNativeFrozen()){
+            println("Natively Frozen")
+        }
+        if(isFrozen()){
+            println("Frozen")
+        }
+        ensureNeverFrozen()
+    }
+
     override val size: Int
-        get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
+        get() = runBlocking {
+            println("Size")
+            access { it.size } }
 
     override fun containsKey(key: K): Boolean = runBlocking {
-        access { it.containsKey(key) }
+        println("Contains Key")
+        access { it.containsKey(key)
+        }
     }
 
-    override fun containsValue(value: V): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun containsValue(value: V): Boolean = runBlocking {
+        println("Value")
+        access { it.containsValue(value)
+        }
     }
 
-    override fun get(key: K): V? {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun get(key: K): V? = runBlocking {
+        println("Get")
+        access { it[key] }
     }
 
-    override fun isEmpty(): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun isEmpty(): Boolean = runBlocking {
+        println("empty")
+        access { it.isEmpty()
+        }
     }
 
     override val entries: MutableSet<MutableMap.MutableEntry<K, V>>
-        get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
+        get() =  runBlocking {
+            println("Entries")
+            access { it.entries }
+        }
     override val keys: MutableSet<K>
-        get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
+        get() =  runBlocking {
+            println("Keys")
+            access { it.keys }
+        }
     override val values: MutableCollection<V>
-        get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
+        get() = runBlocking {
+            println("Values")
+            access { it.values }
+        }
 
-    override fun clear() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun clear() = runBlocking {
+        println("Clear")
+        access { it.clear() }
     }
 
-    override fun put(key: K, value: V): V? {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun put(key: K, value: V): V? = runBlocking {
+        println("put")
+        access { it.put(key,value) }
     }
 
-    override fun putAll(from: Map<out K, V>) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun putAll(from: Map<out K, V>) = runBlocking {
+        println("put all")
+        access { it.putAll(from) }
     }
 
-    override fun remove(key: K): V? {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun remove(key: K): V? = runBlocking {
+        println("remove")
+        access { it.remove(key) }
     }
 
 }
