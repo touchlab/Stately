a266403a5e80a71198a645eb4b47a7d83e4dace4
Kevin Galligan <kgalligan@gmail.com>
2021-03-27 11:09:35 -0400

    Use AtomicBoolean

--- stately-isolate/src/jvmMain/kotlin/co/touchlab/stately/isolate/StateHolder.kt
+++ stately-isolate/src/jvmMain/kotlin/co/touchlab/stately/isolate/StateHolder.kt
@@ -1,17 +1,18 @@
 package co.touchlab.stately.isolate
 
 import co.touchlab.stately.concurrency.ThreadRef
+import java.util.concurrent.atomic.AtomicBoolean
 
 actual class StateHolder<out T : Any> internal actual constructor(t: T, actual val stateRunner: StateRunner) {
     actual val myState: T = t
 
-    private var _isDisposed: Boolean = false
+    private var _isDisposed: AtomicBoolean = AtomicBoolean(false)
 
     actual val isDisposed: Boolean
-        get() = _isDisposed
+        get() = _isDisposed.get()
 
     actual fun dispose() {
-        if (!isDisposed) _isDisposed = true
+        _isDisposed.set(true)
     }
 
     private val threadRef = ThreadRef()
