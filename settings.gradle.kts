b1942d8981a74da817cd311961b8114d5b5461d6
Kevin Galligan <kgalligan@gmail.com>
2022-07-23 09:21:03 -0400

    Moved folders

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,3 +4,8 @@ include(":stately-concurrency")
 include(":stately-collections")
 include(":stately-isolate")
 include(":stately-iso-collections")
+
+project(":stately-common").projectDir = File("deprecated/stately-common")
+project(":stately-collections").projectDir = File("deprecated/stately-collections")
+project(":stately-isolate").projectDir = File("deprecated/stately-isolate")
+project(":stately-iso-collections").projectDir = File("deprecated/stately-iso-collections")
